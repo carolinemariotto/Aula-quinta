c2fcac1298bb3f175d0527526e1ef12cc9b1698f
JulioVianaDev <juliovianadev@gmail.com>
2022-02-03 15:26:05 -0300

    adicionando uma imagem

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import './navbar.css';
-
+import tomate from '../imagens/tomate.png';
 function Navbar (){
     return(
         <nav class="navbar navbar-expand-lg navbar-light navbar-site">
             <div class="container-fluid">
+                <img src={tomate}/>
                 <a class="Tomate" href="#">Tomate</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
